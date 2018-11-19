d5750ec02e26fad19165e7d92bcbb9a59889f5ae
Csaba Gyuris <gycsaba@vasvari.hu>
2018-11-19 12:37:22 +0100

    Vezérlő osztály fájlból beolvasott valós számok kezelésére.

---
+++ OOPControllerDoubleData/Controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ControllerDoubleDataProcessing
+{
+    class Controller
+    {
+        List<Double> subjects;
+        public Controller()
+        {
+            subjects = new List<Double>();
+        }
+        public void readFromFile()
+        {
+            StreamReader sr = new StreamReader("adatok.txt", Encoding.UTF8);
+            while(!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                double a = Convert.ToDouble(line);
+                subjects.Add(a);
+            }
+        }
+        public int getNumberOfSubject()
+        {
+            return subjects.Count();
+        }
+    }
+}
